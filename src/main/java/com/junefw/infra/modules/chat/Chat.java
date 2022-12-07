a14f324354a2cc70d83fed97df3dd80bb92d39c6
excitingmvr <excitingmvr@gmail.com>
2022-12-07 10:23:49 +0900

    asdf

---
+++ src/main/java/com/junefw/infra/modules/chat/Chat.java
@@ -0,0 +1,67 @@
+package com.junefw.infra.modules.chat;
+//본인에게 맞는 package로 변경하도록 합니다.
+
+public class Chat {
+	
+	private Integer chatSeq;
+	private Integer chatDelNy;
+	private Integer cuMember;
+	private String chatCreateDate;
+	
+	//채팅 맴버 정보 추출을 위한 필드
+	//아래 필드도 본인의 DB테이블에 맞게 변경해주세요
+	private Integer ifmmSeq;       //유저 시퀀스
+	private String ifmmName;   //유저 이름 or 닉네임
+	private String upPath;       //프로필 이미지 경로
+	private String upUuidName;   //프로필 이미지 파일 이름
+	
+	public Integer getChatSeq() {
+		return chatSeq;
+	}
+	public void setChatSeq(Integer chatSeq) {
+		this.chatSeq = chatSeq;
+	}
+	public Integer getChatDelNy() {
+		return chatDelNy;
+	}
+	public void setChatDelNy(Integer chatDelNy) {
+		this.chatDelNy = chatDelNy;
+	}
+	public Integer getCuMember() {
+		return cuMember;
+	}
+	public void setCuMember(Integer cuMember) {
+		this.cuMember = cuMember;
+	}
+	public String getChatCreateDate() {
+		return chatCreateDate;
+	}
+	public void setChatCreateDate(String chatCreateDate) {
+		this.chatCreateDate = chatCreateDate;
+	}
+	public Integer getIfmmSeq() {
+		return ifmmSeq;
+	}
+	public void setIfmmSeq(Integer ifmmSeq) {
+		this.ifmmSeq = ifmmSeq;
+	}
+	public String getIfmmName() {
+		return ifmmName;
+	}
+	public void setIfmmName(String ifmmName) {
+		this.ifmmName = ifmmName;
+	}
+	public String getUpPath() {
+		return upPath;
+	}
+	public void setUpPath(String upPath) {
+		this.upPath = upPath;
+	}
+	public String getUpUuidName() {
+		return upUuidName;
+	}
+	public void setUpUuidName(String upUuidName) {
+		this.upUuidName = upUuidName;
+	}
+	
+}
